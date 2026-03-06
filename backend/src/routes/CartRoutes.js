5416ae63b470b9ac231dc343f913fe0968e0cbaf
HuangKha097 <huangkha097@gmail.com>
2026-03-06 21:16:31 +0700

    Merge code branches to main

--- backend/src/routes/CartRoutes.js
+++ backend/src/routes/CartRoutes.js
@@ -5,10 +5,7 @@ import { authenToken } from "../middlewares/AuthenToken.js";
 
 const router = express.Router();
 
-// --- PRIVATE ROUTES ---
 router.post('/add', authenToken, addToCart);
-
-// Hỗ trợ cả req.params.userId và truyền qua header x-client-id
 router.get('/', authenToken, getCart);
 router.get('/:userId', authenToken, getCart);
 
