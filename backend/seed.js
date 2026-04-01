c7f613e961359a1840d4bc2c43216f11e8b507b9
laithethanh <laithanh@1006@gmail.com>
2026-04-01 10:52:16 +0700

    fix bug in module shop controller

--- backend/seed.js
+++ backend/seed.js
@@ -5,11 +5,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+  
 await mongoose.connect(process.env.MONGODB_URI);
 console.log("MongoDB Connected");
 
 // ===== INSERT SHOP =====
+
 const shop = await mongoose.connection.collection("Shops").insertOne({
   owner: new mongoose.Types.ObjectId(),
   name: "Shop Test ABC",
